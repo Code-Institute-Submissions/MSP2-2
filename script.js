6e08fbb98fd3d38c458214216137194d74b18aff
Sparkie1387 <hawkesb77@gmail.com>
2020-10-12 22:31:57 +0000

    Added flip, unflip, match and no match to js

--- script.js
+++ script.js
@@ -10,14 +10,34 @@ function flipCard() {
     //first card
     hasFlippedCard = true;
     firstCard = this;
-  } else {
-     // second click
+
+    return;
+  }  
+   // second click
       hasFlippedCard = false;
       secondCard = this;
 
-      // do cards match?
-      
+     checkForMatch();  
   }
+
+
+function checkForMatch () {
+    let isMatch = firstCard.dataset.framework ===
+        secondCard.dataset.framework
+
+        isMatch ? disableCards() : unflipCards();
+}
+
+function disableCard() {
+    firstCard.removeEventListener('click', flipcard);
+    secondCard.removeEventListener('click', flipCard);
+}
+
+function unflipCards() {
+            setTimeout(() => {
+            firstCard.classList.remove('flip');
+            secondCard.classList.remove('flip');
+       }, 1500);
 }
 
 cards.forEach(card => card.addEventListener('click', flipCard))
